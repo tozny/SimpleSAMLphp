7bc634f31b3467e9f33c8fe61318479bc42cb2ec
Ben Reichert <ben@tozny.com>
2017-06-23 15:24:02 -0700

    add ability to turn off cert generation, a new sp-remote template, remove examples from idp-hosted template

--- docker-tozny/docker-entrypoint.sh
+++ docker-tozny/docker-entrypoint.sh
@@ -5,7 +5,10 @@ if [ ! -f /params.yml ] ; then
 	cat /params.default.yml >> /params.yml
 fi
 
-cd /srv/simplesaml/cert && openssl req -new -x509 -days 3652 -nodes -out server.cert -keyout server.pem -subj "/C=US/ST=Oregon/L=Portland/O=Tozny SimpleSAMLphp/OU=IT Department/CN=tozny.com"
+# only generate cert if the GEN_CERT env var is set (doesn't matter with what though)
+if [ -z "$GEN_CERT" ]; then
+  cd /srv/simplesaml/cert && openssl req -new -x509 -days 3652 -nodes -out server.cert -keyout server.pem -subj "/C=US/ST=Oregon/L=Portland/O=Tozny SimpleSAMLphp/OU=IT Department/CN=tozny.com"
+fi
 
 twit /srv/templates/authsources.php.tpl /srv/simplesaml/config/authsources.php \
 	--params=/params.default.yml --params=/params.yml --no-escape
